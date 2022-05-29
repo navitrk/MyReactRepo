b399a1f576065193f3752b951d5dba6cdb6cbfd6
navitrk <navitrk@gmail.com>
2022-05-30 00:22:43 +0530

    Hi

---
+++ src/SideNavBar/SideNavBar.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./SideNavBar.css";
+
+const SideNavBar = () => {
+	const [isExpanded, setExpendState] = useState(false);
+	const menuItems = [
+		{
+			text: "Dashboard",
+			icon: "icons/grid.svg",
+		},
+		{
+			text: "India",
+			
+		},
+		{
+			text: "Karnataka",
+			
+		},
+		{
+			text: "Health",
+			
+		},
+		{
+			text: "Sports",
+			
+		},
+		{
+			text: "Business",
+			
+		},
+		{
+			text: "Market",
+			
+		},
+		
+	];
+	return (
+		<div
+			className={
+				isExpanded
+					? "side-nav-container"
+					: "side-nav-container side-nav-container-NX"
+			}
+		>
+			<div className="nav-upper">
+				<div className="nav-heading">
+					{isExpanded && (
+						<div className="nav-brand">
+							<img src="icons/Logo.svg" alt="" srcset="" />
+							<h2>Navi News</h2>
+						</div>
+					)}
+					<button
+						className={
+							isExpanded ? "hamburger hamburger-in" : "hamburger hamburger-out"
+						}
+						onClick={() => setExpendState(!isExpanded)}
+					>
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+				</div>
+				<div className="nav-menu">
+					{menuItems.map(({ text, icon }) => (
+						<a
+							className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+							href="#"
+						>
+							<img className="menu-item-icon" src={icon} alt="" srcset="" />
+							{isExpanded && <p>{text}</p>}
+						</a>
+					))}
+				</div>
+			</div>
+			
+		</div>
+	);
+};
+
+export default SideNavBar;
